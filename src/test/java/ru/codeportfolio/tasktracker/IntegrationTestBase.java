bb8d96d03796034eed7416256500ede63450df7a
artemka2003 <86artem2003@mail.ru>
2026-08-11 21:53:39 +0300

    fix tests in gradlew

--- src/test/java/ru/codeportfolio/tasktracker/IntegrationTestBase.java
+++ src/test/java/ru/codeportfolio/tasktracker/IntegrationTestBase.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -15,37 +16,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class IntegrationTestBase {
-
-/*    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }*/
 
     @Container
     static PostgreSQLContainer<?> postgres =
-            new PostgreSQLContainer<>("postgres:16-alpine")
-                    ;
-
-    static {
-        postgres.start();
-    }
-
-    @BeforeAll
-    static void checkContainer() {
-        System.out.println("POSTGRES RUNNING: " + postgres.isRunning());
-        System.out.println("JDBC URL: " + postgres.getJdbcUrl());
-    }
+            new PostgreSQLContainer<>("postgres:16-alpine");
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
-        System.out.println("=== CONTAINER RUNNING: " + postgres.isRunning());
-        System.out.println("=== JDBC URL: " + postgres.getJdbcUrl());
 
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
