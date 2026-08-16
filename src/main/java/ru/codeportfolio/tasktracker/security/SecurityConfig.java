97ecb441c76795f1e7260746b13c09c090b2fe2b
artemka2003 <86artem2003@mail.ru>
2026-08-16 18:40:13 +0300

    fix: permit health check in security

--- src/main/java/ru/codeportfolio/tasktracker/security/SecurityConfig.java
+++ src/main/java/ru/codeportfolio/tasktracker/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**"
+                        .requestMatchers("/auth/**", "/actuator/health"
                         ).permitAll()
                         .requestMatchers(HttpMethod.POST, "/user").permitAll()
                         .requestMatchers(HttpMethod.GET, "/user").authenticated()
